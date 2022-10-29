df79058fd626fda726fc1f11514b41acc640c0fb
Pedro Andrade <pedro.henrique.ao16@gmail.com>
2022-10-29 01:47:21 -0300

    translate

---
+++ lang/pt-BR/pagination.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linhas de idioma de paginação
+    |--------------------------------------------------------------------------
+    |
+    | As linhas de linguagem a seguir são usadas pela biblioteca do paginador
+    | para criar os links de paginação simples. Você é livre para alterá-los
+    | para qualquer coisa que desejar para personalizar suas visualizações para
+    | melhor corresponder ao seu aplicativo.
+    |
+    */
+
+    'previous' => '&laquo; Anterior',
+    'next' => 'Próximo &raquo;',
+
+];
---
+++ lang/pt-BR/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linhas de idioma de redefinição de senha
+    |--------------------------------------------------------------------------
+    |
+    | As linhas de idioma a seguir são as linhas padrão que correspondem aos
+    | motivos fornecidos pelo agente de senha para uma tentativa de atualização
+    | de senha que falhou, como um token inválido ou uma nova senha inválida.
+    |
+    */
+
+    'reset' => 'Sua senha foi redefinida!',
+    'sent' => 'Enviamos seu link de redefinição de senha por e-mail!',
+    'throttled' => 'Aguarde antes de tentar novamente.',
+    'token' => 'Este token de redefinição de senha é inválido.',
+    'user' => "Não encontramos um usuário com esse endereço de e-mail.",
+
+];
